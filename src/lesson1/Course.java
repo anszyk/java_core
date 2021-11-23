d5fdf6c6520c36612553efb19fe2647beea6ae5a
anszyk <anszyk0@gmail.com>
2021-11-23 22:34:52 +0700

    3

--- src/lesson1/Course.java
+++ src/lesson1/Course.java
@@ -4,5 +4,7 @@ public class Course {
 
     public static void main(String[] args){
         System.out.println("Hello, Vitya!");
+        System.out.println("Hello, Vitya!");
+        System.out.println("Hello, Vitya!");
     }
 }
